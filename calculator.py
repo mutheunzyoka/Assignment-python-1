45d5fa4de109b265037729da9a9326016f8ef3cc
mutheunzyoka <nzyokagladys17@gmail.com>
2025-03-14 14:46:52 +0300

    initial commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first no: "))
+b = int(input("Enter second no: "))
+sum = a+b
+sub= a-b
+multi= a*b
+div=a/b
+print("Sum :", sum)
+print("Subtraction: ",sub)
+print("Multipication: ",multi)
+print("Division: ",div)
